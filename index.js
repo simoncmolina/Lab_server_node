46c07e13a580c2077a4212b2e773054ca73e5ee6
simoncmolina <simoncoral_10@hotmail.com>
2023-09-18 17:43:17 -0500

    Agregado chalk y texto JSON

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const host = '127.0.0.1';
 const port = '8000';
 
 let server = http.createServer(function(call, response){
-    response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.end({
+    response.writeHead(200, {'Content-Type' : 'application/json'});
+    response.end(JSON.stringify({
         "nombre":"Simón",
-        "mensaje":"Estamos conduciendo una buena estrategia"
-    });
+        "mensaje":"Hola Simón"
+    }));
 });
 
 server.listen(port, host, function(){
-    console.log('Server running: ' + host + ':' + port);
+    console.log(chalk.green('Server running: ' + host + ':' + port));
 })
